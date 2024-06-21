fed2880e42ec8090909738ca85f00aa50dded7fa
mina magdy <120422463+mina20028@users.noreply.github.com>
2024-06-21 03:30:55 +0300

    edit

--- App.js
+++ App.js
@@ -12,6 +12,13 @@ import Tapbar from './Screens/Tapbar';
 import ProfilePage from './Screens/Profile';
 import HomeProfile from './Screens/HomeProfile';
 import Home from './Screens/Home';
+import Welcom from './Screens/Welcom';
+import ResetPassword from './Screens/ResetPassword';
+import HomeRevU from './Screens/HomeRevU';
+import ProductsReviews from './Screens/ProductsReviews';
+import ClassifyReview from './Screens/ClassifyReview';
+import PositiveandNegative from './Screens/PositiveandNegative';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -26,6 +33,14 @@ export default function App() {
 
 
         <Stack.Screen
+          name="Splash"
+          component={Splash}
+        />
+        <Stack.Screen
+          name="Welcom"
+          component={Welcom}
+        />
+        <Stack.Screen
           name="Login"
           component={Login}
         />
@@ -33,6 +48,33 @@ export default function App() {
           name="Tapbar"
           component={Tapbar}
         />
+        <Stack.Screen
+          name="PositiveandNegative"
+          component={PositiveandNegative}
+        />
+
+        <Stack.Screen
+          name="ClassifyReview"
+          component={ClassifyReview}
+        />
+
+        <Stack.Screen
+          name="ProductsReviews"
+          component={ProductsReviews}
+        />
+        <Stack.Screen
+          name="ResetPassword"
+          component={ResetPassword}
+        />
+        {/* <Stack.Screen
+          name="Tapbar"
+          component={Tapbar}
+        /> */}
+        <Stack.Screen
+          name="HomeRevU"
+          component={HomeRevU}
+        />
+
         <Stack.Screen
           name="Home"
           component={Home}
@@ -52,11 +94,6 @@ export default function App() {
         <Stack.Screen
           name="Onboarding"
           component={Onboarding}
-        />
-
-        <Stack.Screen
-          name="Splash"
-          component={Splash}
         />
 
         <Stack.Screen
